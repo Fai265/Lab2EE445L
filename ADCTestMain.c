7204432df21f91f9943242025c2f591eee3a95a3
CooperCarnahan <coopcarn@gmail.com>
2017-09-15 21:40:38 -0500

    Fixed some of buffer_functions and changed ADCTime
    ISR needs work b/c disabling interrupts seems potentially risky.
    #TODO: Need to change how

--- ADCTestMain.c
+++ ADCTestMain.c
@@ -45,6 +45,8 @@ static uint32_t currentTime = 0;
 static int index = 0;
 static int ADCTime[1000];
 static int ADCData[1000];
+static short jitter=0;
+static short pmf_array[4096];
 // This debug function initializes Timer0A to request interrupts
 // at a 100 Hz frequency.  It is similar to FreqMeasure.c.
 void Timer0A_Init100HzInt(void){
@@ -96,7 +98,15 @@ void Timer0A_Handler(void){
 	if(index < 1000){
 	  ADCData[index] = ADCvalue;
 		ADCTime[index] = (startTime - currentTime);
+		startTime=currentTime;
 		index++;
+		
+	}
+	else{
+		DisableInterrupts();
+		jitter=Jitter_Calc(ADCTime);
+		PMF_Create(ADCData,pmf_array);
+	
 	}
 }
 
@@ -106,16 +116,18 @@ int main(void){
   PLL_Init(Bus80MHz);                   // 80 MHz
   SYSCTL_RCGCGPIO_R |= 0x20;            // activate port F
   ADC0_InitSWTriggerSeq3_Ch9();         // allow time to finish activating
-	Timer1_Init();
+	Timer1_Init();												// Sets up Timer1 count for ~53 seconds to time duration in between ADC reads
   Timer0A_Init100HzInt();               // set up Timer0A for 100 Hz interrupts
   GPIO_PORTF_DIR_R |= 0x06;             // make PF2, PF1 out (built-in LED)
   GPIO_PORTF_AFSEL_R &= ~0x06;          // disable alt funct on PF2, PF1
   GPIO_PORTF_DEN_R |= 0x06;             // enable digital I/O on PF2, PF1
   GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFFFF00F)+0x00000000; // configure PF2 as GPIO
   GPIO_PORTF_AMSEL_R = 0;               // disable analog functionality on PF
-  PF2 = 0;                      // turn off LED
+  PF2 = 0;                              // turn off LED
   EnableInterrupts();
 	
+
+		
   while(1){
     GPIO_PORTF_DATA_R ^= 0x02;
   }
--- BufferFunctions.c
+++ BufferFunctions.c
@@ -1,19 +1,19 @@
 #include <stdint.h>
 #include "ST7735.h"
 
-static short PMFArray[4096];
+//static short PMFArray[4096];
 
 short Jitter_Calc(int* ADCTime){ //can edit input parameter if we move buffer back to main
 	short maxDiff=0;
   for(int i=0;i<999;i++){
-	  if((ADCTime[i]-ADCTime[i+1])>maxDiff){
-		  maxDiff=ADCTime[i]-ADCTime[i+1];
+	  if(abs(ADCTime[i]-ADCTime[i+1])>maxDiff){
+		  maxDiff=abs(ADCTime[i]-ADCTime[i+1]);
 		}
 	}
 	return maxDiff;
 }
 
-short* PMF_Create(int* ADCData){
+short* PMF_Create(int* ADCData, short* PMFArray){
 	short adcRange;
 	short adcMin;
 	short adcMax;
